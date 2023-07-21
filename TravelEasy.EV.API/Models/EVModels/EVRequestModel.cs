09cbef8c43963c8b1c78354eec7b481663184b25
Anton Dimitrov <antonvd680@gmail.com>
2023-07-21 01:45:45 -0700

    + added EV request model + added EV request all model + organised request/response models in folders

---
+++ TravelEasy.EV.API/Models/EVModels/EVRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelEasy.EV.API.Models.EVModels
+{
+    public class EVRequestModel
+    {
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? HorsePower { get; set; }
+        public string? Range { get; set; }
+        public string? PricePerDay { get; set; }
+    }
+}
